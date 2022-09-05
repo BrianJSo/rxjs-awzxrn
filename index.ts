6a24101e2b5a46a7667b3ed554dd6ff949f789e8
BrianJSo <51768646+BrianJSo@users.noreply.github.com>
2022-09-05 16:15:37 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
 import { fromEvent } from 'rxjs';
-import { exhaustMap, map, takeUntil } from 'rxjs/operators';
+import { fromFetch } from 'rxjs/fetch';
+import {
+  map,
+  debounceTime,
+  throttleTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs/operators';
 
 const search = document.getElementById('searchbar');
 
 const keyup$ = fromEvent(search, 'keyup');
-keyup$.subscribe((evt) => {
-  console.log((evt.target as HTMLInputElement).value);
-});
+
+keyup$
+  .pipe(
+    throttleTime(300),
+    map((evt: KeyboardEvent) => (evt.target as HTMLInputElement).value),
+    distinctUntilChanged(),
+    filter((value) => value.length > 2),
+    switchMap((value) => {
+      console.log('hello');
+      return fromFetch('https://api.jikan.moe/v4/anime?q=' + value).pipe(
+        switchMap((response) => response.json())
+      );
+    })
+  )
+  .subscribe((value) => {
+    console.log(value);
+  });
